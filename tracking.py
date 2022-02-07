f096c74300543d4eb68e2f5e93240bacf723b8c3
Dzmitry <55855155+dzmitryZhuk@users.noreply.github.com>
2022-02-07 07:39:15 +0300

    realize #4

--- tracking.py
+++ tracking.py
@@ -6,8 +6,8 @@ import datetime
 
 START = "start>"
 STOP = "stop>"
+PROGRAMS = ["AnyDesk"]
 DIRECTORY = os.path.dirname(os.path.realpath(__file__)) + "\\data\\"
-TARGET_PROGRAM_NAME = "AnyDesk"
 
 # add to startup
 def startup():
@@ -18,55 +18,73 @@ def startup():
         swl.create_lnk(os.path.realpath(__file__), path_link)
         print("Added to startup")
 
-# make directory where will be all info aboat working time
+# create directory where will be all info aboat working time
 def dir():
     if not os.path.exists(DIRECTORY):
         os.makedirs(DIRECTORY)
-        print("Target folder created")
+        print("folder for data created")
+    if len(PROGRAMS) > 0:
+        for name in PROGRAMS:
+            subdir(name)
+
+# create subdirectory for program
+def subdir(name):
+    if not os.path.exists(DIRECTORY + '\\' + name + '\\'):
+        os.makedirs(DIRECTORY + '\\' + name + '\\')
+        print("\'" + name + "\' folder created")
 
 #########################################################################################
 
-was_started = False
+was_started = {}
+for prog in PROGRAMS:
+    was_started[prog] = False
 current_day = datetime.datetime.now().date().strftime("%d")
 
 startup()
 while True:
     dir()
     now = datetime.datetime.now()
+    if len(PROGRAMS) > 0:
+        # if u are still working but it's a new day
+        temp_day = now.date().strftime("%d")
+        if temp_day != current_day:
+            for program_name in PROGRAMS:
+                if was_started[program_name] == True:
+                    file = open(DIRECTORY + '\\' + program_name + '\\' + now.date().strftime("%Y_%m_") + current_day, 'a')
+                    file.write(STOP + " " + "23:59:59" + '\n')
+                    file.close()
+                    
+                    current_day = temp_day
 
-    # if u are still working but it's a new day
-    temp_day = now.date().strftime("%d")
-    if temp_day != current_day and was_started == True:
-        file = open(DIRECTORY + now.date().strftime("%Y_%m_") + current_day, 'a')
-        file.write(STOP + " " + "23:59:59" + '\n')
-        file.close()
+                    file = open(DIRECTORY + '\\' + program_name + '\\' + now.date().strftime("%Y_%m_") + current_day, 'a')
+                    file.write(START + " " + now.time().strftime("%H:%M:%S") + '\n')
+                    file.close()
+
+        # check if programm is running
+        process_running = {}
+        for prog in PROGRAMS:
+            process_running[prog] = False
+        windows = Desktop(backend="uia").windows()
+        for w in windows:
+            name = w.window_text()
+            for program_name in PROGRAMS:
+                if program_name.lower() in name.lower():
+                    process_running[program_name] = True
         
-        current_day = temp_day
+        # start working
+        for program_name in PROGRAMS:
+            if process_running[program_name] == True and was_started[program_name] == False:
+                was_started[program_name] = True
+                file = open(DIRECTORY + '\\' + program_name + '\\' + now.date().strftime("%Y_%m_") + current_day, 'a')
+                file.write(START + " " + now.time().strftime("%H:%M:%S") + '\n')
+                file.close()
 
-        file = open(DIRECTORY + now.date().strftime("%Y_%m_") + current_day, 'a')
-        file.write(START + " " + now.time().strftime("%H:%M:%S") + '\n')
-        file.close()
-
-    # check if programm is running
-    process_running = False
-    windows = Desktop(backend="uia").windows()
-    for w in windows:
-        name = w.window_text()
-        if TARGET_PROGRAM_NAME.lower() in name.lower():
-            process_running = True
-    
-    # start working
-    if process_running == True and was_started == False:
-        was_started = True
-        file = open(DIRECTORY + now.date().strftime("%Y_%m_") + current_day, 'a')
-        file.write(START + " " + now.time().strftime("%H:%M:%S") + '\n')
-        file.close()
-
-    # stop working
-    if process_running == False and was_started == True:
-        was_started = False
-        file = open(DIRECTORY + now.date().strftime("%Y_%m_") + current_day, 'a')
-        file.write(STOP + " " + now.time().strftime("%H:%M:%S") + '\n')
-        file.close()    
+        # stop working
+        for program_name in PROGRAMS:
+            if process_running[program_name] == False and was_started[program_name] == True:
+                was_started[program_name] = False
+                file = open(DIRECTORY + '\\' + program_name + '\\' + now.date().strftime("%Y_%m_") + current_day, 'a')
+                file.write(STOP + " " + now.time().strftime("%H:%M:%S") + '\n')
+                file.close()    
 
     time.sleep(1)
